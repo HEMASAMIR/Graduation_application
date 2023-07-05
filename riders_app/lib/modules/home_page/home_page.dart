5c0edc6a165ba376dc8d068db3fccdfb220b155f
Mohamad amgad elsaid <mohamadamgad09@gmail.com>
2023-07-05 15:18:37 +0300

    drawer added by AMG

--- riders_app/lib/modules/home_page/home_page.dart
+++ riders_app/lib/modules/home_page/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:riders_app/services/user_db.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -6,8 +7,54 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: Drawer(
+        child: Column(
+          children: [
+            UserAccountsDrawerHeader(
+              currentAccountPicture:
+                  Image.network(UserDatabase.instance.getImageUrl),
+              accountName: Text("${UserDatabase.instance.getUserName}"),
+              accountEmail: Text("${UserDatabase.instance.getEmail}"),
+            ),
+            ListTile(
+              style: ListTileStyle.drawer,
+              onTap: () {},
+              title: Text("Home"),
+              trailing: Icon(Icons.home),
+            ),
+            ListTile(
+              style: ListTileStyle.drawer,
+              onTap: () {},
+              title: Text("Home"),
+              trailing: Icon(Icons.home),
+            ),
+            ListTile(
+              style: ListTileStyle.drawer,
+              onTap: () {},
+              title: Text("Home"),
+              trailing: Icon(Icons.home),
+            ),
+            ListTile(
+              style: ListTileStyle.drawer,
+              onTap: () {},
+              title: Text("Home"),
+              trailing: Icon(Icons.home),
+            ),
+          ],
+        ),
+      ),
       appBar: AppBar(
-        automaticallyImplyLeading: false,
+        title: Row(
+          children: [
+            // CircleAvatar(
+            //   backgroundImage: NetworkImage(UserDatabase.instance.getImageUrl),
+            // ),
+            SizedBox(
+              width: 8,
+            ),
+            Text('Welcome back , ${UserDatabase.instance.getUserName}')
+          ],
+        ),
       ),
     );
   }
